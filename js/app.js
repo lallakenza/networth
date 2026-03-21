899b60bbf3667ec5c20fc31f4578c8bfc794827d
lallakenza <amine.koraibi@gmail.com>
2026-03-21 18:39:41 +0000

    v178: Weekly sampling for 1Y chart (~52 points instead of ~250)

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ import { PORTFOLIO, FX_STATIC, DATA_LAST_UPDATE } from './data.js?v=176';
 import { compute } from './engine.js?v=176';
 import { render } from './render.js?v=176';
 import { fetchFXRates, fetchStockPrices, retryFailedTickers, fetchSoldStockPrices, clearCache, fetchHistoricalPricesYTD, fetchHistoricalPrices1Y } from './api.js?v=176';
-import { rebuildAllCharts, buildCFProjection, coupleChartZoomOut, buildPortfolioYTDChart, redrawChartForPeriod } from './charts.js?v=177';
+import { rebuildAllCharts, buildCFProjection, coupleChartZoomOut, buildPortfolioYTDChart, redrawChartForPeriod } from './charts.js?v=178';
 import { initSimulators, bindSimulatorEvents } from './simulators.js?v=176';
 
 // ---- App state ----
